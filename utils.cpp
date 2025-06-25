529adb533139c55c7c1d9df1dca5312fc1fea9b7
Ziad Eliwa <ziadeliwa@aucegypt.edu>
2025-06-25 19:15:51 +0300

    Implemented the main menu

--- utils.cpp
+++ utils.cpp
@@ -1,5 +1,42 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-void login();
-void registering();
-void MainMenu();
+void login()
+{
+    
+}
+void registering()
+{
+
+}
+void MainMenu()
+{
+    bool exit = true;
+    do
+    {
+        cout << "Main Menu, Welcome to the Firefighting system!";
+        cout << "-----------------------------------------------\n";
+    
+        cout << "(1) Register\n";
+        cout << "(2) Login\n";
+        cout << "Enter your choice: ";
+        int choice;
+        cin >> choice;
+    
+        switch (choice)
+        {
+        case 1: // login 
+            login();
+            exit = false;
+            break;
+        case 2: // register
+            registering();
+            exit = false;   
+            break;
+        default:
+            cout << "Wrong Input, Please enter a correct choice. ";           
+            break;
+        }
+    } while (exit);
+    
+}
